1a3036234f61851e41c5963a9ef783a8338eef00
trgiang1210 <trgiang12102006@gmail.com>
2025-10-22 15:47:39 +0700

    sua lai class expand

--- ExpandPaddlePowerUp.java
+++ ExpandPaddlePowerUp.java
@@ -1,29 +1,33 @@
-public abstract class ExpandPaddlePowerUp extends PowerUp{
-    // tăng kích thước paddle
+package org.example.demo;
+
+// 1. Xóa 'abstract'
+public class ExpandPaddlePowerUp extends PowerUp {
 
     public int expandAmount = 50;
+    private boolean effectApplied = false; // biến để theo dõi powerup
 
     public ExpandPaddlePowerUp(double x, double y) {
-        super(x, y, 20, 20, "ExpandPaddle", 5000); // 5 giây hiệu lực
+        super(x, y, 20, 20, "ExpandPaddle", 10000); // 5 giây
     }
 
-
-    public void applyEffect(Paddle paddle) {
-        if (Paddle.currentPowerUp == null) {
+    @Override
+    public void applyEffect(Paddle paddle, Ball ball) {
+        // chỉ áp dụng hiệu ứng nếu nó chưa được áp dụng
+        if (!effectApplied) {
             paddle.setWidth(paddle.getWidth() + expandAmount);
-            active = true;
-            System.out.println("Tăng kích tươớc paddle");
+            effectApplied = true;
+            System.out.println("Tăng kích thước paddle");
         }
     }
 
-
-    public void removeEffect(Paddle paddle) {
-        if (active) {
+    @Override
+    public void removeEffect(Paddle paddle, Ball ball) {
+        // Chỉ gỡ bỏ hiệu ứng nếu nó đã được áp dụng
+        if (effectApplied) {
             paddle.setWidth(paddle.getWidth() - expandAmount);
-            active = false;
+            effectApplied = false;
+            this.setActive(false); // Đánh dấu power-up này là "chết" hoàn toàn
             System.out.println("Hết hiệu lực");
         }
     }
 }
-
-
