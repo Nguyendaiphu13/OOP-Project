25a1b089a465028dcc3b60b5a65a856e071ea6a8
trgiang1210 <trgiang12102006@gmail.com>
2025-10-22 14:41:38 +0700

    them constructor paddle

--- Paddle.java
+++ Paddle.java
@@ -1,3 +1,7 @@
+package org.example.demo;
+
+import javafx.scene.canvas.GraphicsContext;
+
 public class Paddle extends MovableObject {
     protected double speed;
     protected static String currentPowerUp;
@@ -8,11 +12,11 @@ public class Paddle extends MovableObject {
     public double getSpeed() {
         return speed;
     }
-    public Paddle(double x, double y, int width, int height,double dx, double dy, double speed) {
-        super(x, y, width, height, dx, dy);
+    public Paddle(double x, double y, int width, int height,double speed) {
+        super(x, y, width, height);
         this.speed = speed;
-        this.currentPowerUp = null;
     }
+
     public String getCurrentPowerUp() {
         return currentPowerUp;
     }
@@ -31,18 +35,18 @@ public class Paddle extends MovableObject {
                 speed += 2;
                 break;
             case "SlowDown":
-                 speed -= 2;
-                 break;
+                speed -= 2;
+                break;
             default:
                 break;
-                }
-            }
-        public void update() {
-            move();
-            if (x < 0) {
-                x = 0;
-            }
         }
+    }
+    public void update() {
+        move();
+        if (x < 0) {
+            x = 0;
+        }
+    }
     public void moveLeft() {
         dx = -speed;
     }
