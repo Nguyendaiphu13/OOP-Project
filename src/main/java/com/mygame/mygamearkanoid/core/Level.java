5164820d9e65f449e361681944db0174c2ad9430
Phú Nguyễn <phu1307nhankhang@gmail.com>
2025-11-03 22:37:44 +0700

    thêm level 3

--- src/main/java/com/mygame/mygamearkanoid/core/Level.java
+++ src/main/java/com/mygame/mygamearkanoid/core/Level.java
@@ -17,11 +17,11 @@ public class Level {
 
                 String brickType;
                 double r = rand.nextDouble();
-                if (r < 0.15) { // 15%
+                if (r < 0.4) { // 15%
                     brickType = "2normal";
-                } else if (r < 0.30) { // 15%
+                } else if (r < 0.2) { // 15%
                     brickType = "expand";
-                } else if (r < 0.45) { // 15% (THÊM MỚI)
+                } else if (r < 0.2) { // 15% (THÊM MỚI)
                     brickType = "fast_ball";
                 } else { // 55%
                     brickType = "normal";
@@ -31,42 +31,47 @@ public class Level {
             }
         }
     }
-
-    // SỬA HÀM NÀY
     public static void generateLevel2(GameManager gameManager, int rows, int cols, int brickWidth, int brickHeight, double startX, double startY, double gapX, double gapY) {
-        // Sửa lỗi (1): 'random' -> 'Random'
         Random rand = new Random();
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 double x = j * (brickWidth + gapX) + startX;
                 double y = i * (brickHeight + gapY) + startY;
-
-                // SỬA LOGIC (2): Thêm MoveBrick
-                // Hàng 2 và 4 (chỉ số 1 và 3) sẽ là gạch di chuyển
                 if (i == 1 || i == 3) {
-                    // Lấy tốc độ bóng làm tốc độ gạch
                     double speed = StaticFinal.BALL_SPEED;
-                    // Cho gạch di chuyển ngược chiều nhau
                     double direction = (i == 1) ? 1.0 : -1.0;
-
                     gameManager.bricks.add(new MoveBrick(x, y, brickWidth, brickHeight, "normal", speed * direction, 0));
 
                 } else {
-                    // Các hàng khác tạo gạch bình thường
                     String brickType;
                     double r = rand.nextDouble();
-                    if (r < 0.20) {
+                    if (r < 0.4) {
                         brickType = "2normal";
-                    } else if (r < 0.40) {
+                    } else if (r < 0.2) {
                         brickType = "expand";
-                    } else if (r < 0.60) {
+                    } else if (r < 0.2) {
                         brickType = "fast_ball";
                     } else {
                         brickType = "normal";
                     }
                     gameManager.bricks.add(new Brick(x, y, brickWidth, brickHeight, brickType));
                 }
+            }
+        }
+    }
+    public static void generateLevel3(GameManager gameManager, int rows, int cols, int brickWidth, int brickHeight, double startX, double startY, double gapX, double gapY) {
+        Random rand = new Random();
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                double x = j * (brickWidth + gapX) + startX;
+                double y = i * (brickHeight + gapY) + startY;
+                double speed = StaticFinal.BALL_SPEED;
+                double direction = (i == 1) ? 1.0 : -1.0;
+
+                gameManager.bricks.add(new MoveBrick(x, y, brickWidth, brickHeight, "normal", speed * direction, 0));
+
             }
         }
     }
