d0f71ab68678530115a0421c822d6bb7fbf20059
Phú Nguyễn <phu1307nhankhang@gmail.com>
2025-11-03 23:14:22 +0700

    chỉnh lại lv3

--- src/main/java/com/mygame/mygamearkanoid/core/Level.java
+++ src/main/java/com/mygame/mygamearkanoid/core/Level.java
@@ -62,14 +62,18 @@ public class Level {
     }
     public static void generateLevel3(GameManager gameManager, int rows, int cols, int brickWidth, int brickHeight, double startX, double startY, double gapX, double gapY) {
         Random rand = new Random();
-
+        rows = rows + 5;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                double x = j * (brickWidth + gapX) + startX;
+            for (int j = 0; j < cols; j = j + 2) {
+                double x = j * (brickWidth + gapX) + startX ;
                 double y = i * (brickHeight + gapY) + startY;
-                double speed = StaticFinal.BALL_SPEED;
-                double direction = (i == 1) ? 1.0 : -1.0;
-
+                double speed = StaticFinal.BALL_SPEED - 2.0;
+                double direction;
+                if (i % 2 == 0) {
+                    direction = 1;
+                } else {
+                    direction = -1;
+                }
                 gameManager.bricks.add(new MoveBrick(x, y, brickWidth, brickHeight, "normal", speed * direction, 0));
 
             }
